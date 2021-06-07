f23c981c298553aa0c2a0d317d002d276b085edb
Sravani142 <84003407+Sravani142@users.noreply.github.com>
2021-06-07 05:45:18 +0530

    Add files via upload

---
+++ UserDetails.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDetails {
+	private String name,mobileNumber,govId,dob,emailId,password;
+	private List<Travels> travels = new ArrayList<>();
+	public UserDetails(String name, String mobNum, String govId, String dob, String email, String password) {
+		super();
+		this.name = name;
+		this.mobileNumber = mobNum;
+		this.govId = govId;
+		this.dob = dob;
+		this.emailId = email;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobNum() {
+		return mobileNumber;
+	}
+
+	public void setMobNum(String mobNum) {
+		this.mobileNumber = mobNum;
+	}
+
+	public String getGovId() {
+		return govId;
+	}
+
+	public void setGovId(String govId) {
+		this.govId = govId;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getEmail() {
+		return emailId;
+	}
+
+	public void setEmail(String email) {
+		this.emailId = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Travels> getFlightBookings() {	
+		return travels;
+	}
+
+	public void setFlightBookings(Travels journey) {
+		this.travels.add(journey);
+	}
+	
+	
+}
